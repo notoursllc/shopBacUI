17dfbcbf93839f7e422aee1f5e7779ba4c742c09
greg bruins <gbruins@users.noreply.github.com>
2020-09-07 12:52:54 -0700

    fix: Remove @nuxt/dotenv from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,8 +105,7 @@ export default {
     */
     buildModules: [
         // Doc: https://github.com/nuxt-community/eslint-module
-        // '@nuxtjs/eslint-module',
-        ['@nuxtjs/dotenv', isDev ? null : { path: '/etc/secrets' }]
+        // '@nuxtjs/eslint-module'
     ],
 
     /*
