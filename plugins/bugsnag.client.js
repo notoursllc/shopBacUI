067103fc47fa076caae01541bf9b536378f4f82a
gbru <gbru@users.noreply.github.com>
2021-06-23 22:23:49 -0700

    feat: Upgrade bugsnag

--- plugins/bugsnag.client.js
+++ plugins/bugsnag.client.js
@@ -1,15 +1,17 @@
+import Vue from 'vue';
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginVue from '@bugsnag/plugin-vue';
 
 export default (context, inject) => {
-
     Bugsnag.start({
         apiKey: context.$config.BUG_SNAG_API_KEY,
         plugins: [new BugsnagPluginVue()],
         notifyReleaseStages: [ 'production' ]
     });
 
-    inject('bugsnag', Bugsnag);
+    const bugsnagVue = Bugsnag.getPlugin('vue');
+    bugsnagVue.installVueErrorHandler(Vue);
 
+    inject('bugsnag', Bugsnag);
 };
 
