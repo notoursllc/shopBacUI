acf24a6f83fb037d8bfaaf351f6fb5ee8d846b7a
gbru <gbru@users.noreply.github.com>
2021-09-12 21:49:42 -0700

    fix: Tailwind config should contain a path in content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 const figleafTailwindConfig = require('@notoursllc/figleaf/tailwind.config.js');
 
 // Add our own paths:
+// figleafTailwindConfig.purge.enabled = true; //test only
+
 figleafTailwindConfig.purge.content.push(
-    'components/**/*.vue', // this is already defined in figleaf
+    'components/**/*.vue',
     'layouts/**/*.vue',
     'pages/**/*.vue',
     'plugins/**/*.js',
     'nuxt.config.js',
-    'node_modules/@notoursllc/figleaf/components/**/*.vue'
+    'node_modules/@notoursllc/figleaf/components/**/*.vue',
+    'node_modules/@notoursllc/figleaf/layouts/**/*.vue'
 );
 
 module.exports = Object.assign({}, figleafTailwindConfig);
